2682716dc0aac53da7cebcea9642230334480ddc
Will Brown <will@rebeagle.com>
2020-05-20 14:13:22 -0400

    basic cleanup

--- project-setup/setup-project.sh
+++ project-setup/setup-project.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
+set -ex
 DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
-source "$DIR/../.devcontainer/virtualenv-wrapper-init.sh"
 
+source "$DIR/../.devcontainer/virtualenv-wrapper-init.sh"
 mkproject -r "$DIR/project-requirements.txt" myproject
 ipython kernel install --user --name=$(basename $VIRTUAL_ENV)
