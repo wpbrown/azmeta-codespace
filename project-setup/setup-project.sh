a57df6118a408a68c32455615891f130a1d2d3da
Will Brown <will@rebeagle.com>
2020-05-27 15:50:27 -0400

    make project setup reusable

--- project-setup/setup-project.sh
+++ project-setup/setup-project.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-set -ex
 DIR=$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)
+PROJECT_NAME="${1:-myproject}"
 
 source "$DIR/../.devcontainer/virtualenv-wrapper-init.sh"
-mkproject -r "$DIR/project-requirements.txt" myproject
+mkproject -r "$DIR/project-requirements.txt" "$PROJECT_NAME"
 ipython kernel install --user --name=$(basename $VIRTUAL_ENV)
 mkdir .vscode
 cp "$DIR/workspace-settings.json" .vscode/settings.json
